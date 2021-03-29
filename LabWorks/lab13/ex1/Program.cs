77edfceffe7513ff54480631472a28bfc78a1459
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-03-29 23:33:26 +0300

    Create Program.cs

---
+++ LabWorks/lab13/ex1/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvexQuadrangle quad = new ConvexQuadrangle { a=4,b=7,c=6,d=10 };
+
+            
+            quad.AssignmentSides();
+            Console.WriteLine(quad.Perimetr());
+            Console.WriteLine(quad.Square());
+            quad.TypeOfQuadrangle();
+            Console.ReadLine();
+        }
+    }
+    
+}
