b20614b23f8422db0a303564a108c440e9a2be93
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-05-07 23:38:17 +0300

    Add files via upload

---
+++ LabWorks/lab15/Task1/Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public interface ISides
+    {
+        int Sides { get; set; }
+        void AssignmentSides();
+    }
+
+    public interface ICoordinates
+    {
+        int Coordinates { get; set; }
+        void AssignmentCoordinates();
+    }
+}
