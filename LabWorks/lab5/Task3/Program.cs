ff62a999113ab655b918b112a71a92478fdfca25
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-05-07 21:24:55 +0300

    Add files via upload

---
+++ LabWorks/lab5/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Задарна точнiсть=");
+            int E = Convert.ToInt32(Console.ReadLine());
+            double sum = 2;
+            double p = (x - 1) / (x + 1);
+            for (int i = 1; i < E; i++)
+            {
+                p = Math.Pow(p, 2 * i - 1);
+                p *= 1 / (2 * i - 1);
+                sum += p;
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+
+    }
+}
