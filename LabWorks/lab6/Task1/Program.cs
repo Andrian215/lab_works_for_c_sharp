933f479969f81ad50a23271e8e5f52d5e5f9a9bc
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-05-07 21:38:13 +0300

    Add files via upload

---
+++ LabWorks/lab6/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть кількість елементів:\t");
+            int elCount = int.Parse(Console.ReadLine());
+
+            double[] arr = new double[elCount];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{i}:");
+                arr[i] = double.Parse(Console.ReadLine());
+            }
+            double s = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                s += arr[i];
+            }
+            Console.Write(s / elCount);
+            Console.ReadLine();
+        }
+    }
+}
