8839f6bf6ebe0cd05bffb5f3a253abac9ab45261
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-05-07 21:30:15 +0300

    Add files via upload

---
+++ LabWorks/lab5/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x0 = 1;
+            for (int i = 0; i <= 10; i++)
+            {
+                x0 = x0 + 2 * i;
+            }
+            Console.WriteLine("x10={0}", x0);
+            Console.ReadLine();
+        }
+
+
+    }
+}
