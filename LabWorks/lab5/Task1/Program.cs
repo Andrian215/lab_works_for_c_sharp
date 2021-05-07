6d7b65b1aa4c216c725bb0fef4f2ed3eb5bba727
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-05-07 21:21:42 +0300

    Add files via upload

---
+++ LabWorks/lab5/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double sum = a;
+            int i = 0;
+            while (i < n)
+            {
+
+                sum *= a + i;
+                if (n - i == 1)
+                {
+                    sum *= a + i - 1;
+                }
+                i++;
+                i++;
+            }
+            Console.WriteLine(sum);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
