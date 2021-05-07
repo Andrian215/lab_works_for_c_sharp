7464d9bcb866094bee8d184e4eb4c0bdc9498fe8
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-05-07 21:23:18 +0300

    Add files via upload

---
+++ LabWorks/lab5/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            double n = Convert.ToDouble(Console.ReadLine());
+            int sum = 0;
+            while (n > 1)
+            {
+                n = n / 10;
+                sum++;
+            }
+
+            Console.WriteLine($"Кiлькiсть цифр в числi={sum}");
+            Console.ReadLine();
+        }
+
+
+    }
+}
