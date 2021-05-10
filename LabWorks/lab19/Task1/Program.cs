47e17513366738a4c18fb7bba3eb0762a4b644c0
Andrian215 <74683931+Andrian215@users.noreply.github.com>
2021-05-11 00:23:57 +0300

    Add files via upload

---
+++ LabWorks/lab19/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName;
+            Console.Write("FileName=");
+            FileName = Console.ReadLine();
+            StreamReader SR = new StreamReader(FileName);
+
+            double max = double.MinValue;
+            while (!SR.EndOfStream)
+            {
+                double c = double.Parse(SR.ReadLine());
+                if (c > max)
+                {
+                    max = c;
+                }
+            }
+            Console.WriteLine("max=" + max);
+            SR.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabWorks/lab19/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName;
+            Console.Write("FileName:");
+            FileName = Console.ReadLine();
+            StreamReader SR = new StreamReader(FileName);
+            int n = System.IO.File.ReadAllLines(FileName).Length;
+
+            int[] arr = new int[n];
+
+            for (int i = 0; !SR.EndOfStream; i++)
+            {
+                arr[i] = Convert.ToInt32(SR.ReadLine());
+            }
+
+            for (int i = 0; i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            SR.Close();
+            StreamWriter sw = new StreamWriter("NP.dat.txt");
+            Console.Write("FilenamE:");
+            string FileNamE = Console.ReadLine();
+            StreamWriter SW = new StreamWriter(FileNamE);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                    SW.WriteLine(arr[i]);
+                else
+                    sw.WriteLine(arr[i]);
+            }
+
+            SW.Close();
+            sw.Close();
+            Console.ReadLine();
+        }
+    }
+}
+
+
